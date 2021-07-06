74d8016042cf42f5cb20aad70c18199b717d57ac
soniya rana <soniyasonu0703@gmailo.com>
2021-07-06 18:43:17 +0530

    deploying

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 require("dotenv").config({
-  // path: `.env.development`,
-  path: `.env.${process.env.NODE_ENV}`,
+  path: `.env.development`,
+  // path: `.env.${process.env.NODE_ENV}`,
 })
 
 module.exports = {
@@ -32,7 +32,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.jpeg`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-gatsby-cloud`,
